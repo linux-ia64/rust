97db9f662705e8fe559175f0bbdeff6e3a25cf26
Tomas Glozar <tglozar@gmail.com>
2026-06-26 17:58:05 +0200

    Mark IA-64 in sanity check as not present in stage0

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -39,6 +39,8 @@ const STAGE0_MISSING_TARGETS: &[&str] = &[
     // just a dummy comment so the list doesn't get onelined
     "x86_64-unknown-linux-gnumsan",
     "x86_64-unknown-linux-gnutsan",
+    "powerpc64-unknown-linux-gnuelfv2",
+    "ia64-unknown-linux-gnu",
 ];
 
 /// Minimum version threshold for libstdc++ required when using prebuilt LLVM
